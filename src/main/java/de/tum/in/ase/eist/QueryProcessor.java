4b42bc8746af7035a36f68d2ff3f837edc021f31
Simon Karan G <69362542+SimonKaran13@users.noreply.github.com>
2022-07-14 01:47:06 +0200

    can sum now

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -1,20 +1,30 @@
-package de.tum.in.ase.eist;
+package com.develogical;
 
-import org.springframework.stereotype.Service;
+import javax.print.DocFlavor;
+import java.net.URL;
 
-@Service
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
-        if (query.contains("shakespeare")) {
+        if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "Simon";
-        } else { // TODO extend the programm here
-            return "";
         }
+        if (query.toLowerCase().contains("name")) {
+            return "Simon";
+        }
+        if (query.toLowerCase().contains("hello")) {
+            return "world";
+        }
+        if (query.toLowerCase().contains("what is" )) {
+            try {
+                String[] s = query.split(" ");
+                return Integer.toString(Integer.parseInt(s[2]) + Integer.parseInt(s[4]));
+            } catch (Exception e) {
+                return "0";
+            }
+        }
+        return "";
     }
 }
